6cae23827d3f1914b55a19f30f48be4fcf68fdc9
Benjamin <DNAblue2112@gmail.com>
2020-03-27 15:20:43 +1030

    Added todo messages for plans

--- src/main/java/net/lapismc/dispenserloader/DispenserLoader.java
+++ src/main/java/net/lapismc/dispenserloader/DispenserLoader.java
@@ -4,6 +4,12 @@ import net.lapismc.lapiscore.LapisCorePlugin;
 
 public final class DispenserLoader extends LapisCorePlugin {
 
+    //Command to set block for leading from, chest or dispenser, and a command to force the load
+
+    //Needs groups, each group will have one item to load from and many to load to
+
+    //Timed loading e.g. reload every hour
+
     @Override
     public void onEnable() {
         // Plugin startup logic
