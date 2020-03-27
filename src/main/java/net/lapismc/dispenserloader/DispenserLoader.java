15ee3bcf6516dd8639b9a9a0f20d86fb36d713a0
Benjamin <DNAblue2112@gmail.com>
2020-03-27 15:18:38 +1030

    Initial commit

---
+++ src/main/java/net/lapismc/dispenserloader/DispenserLoader.java
@@ -0,0 +1,17 @@
+package net.lapismc.dispenserloader;
+
+import net.lapismc.lapiscore.LapisCorePlugin;
+
+public final class DispenserLoader extends LapisCorePlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
